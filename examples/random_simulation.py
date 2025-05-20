50955dfb86c86f86104dccb022dab233de5e3888
Ege Altıok <egealtiok@gmail.com>
2025-05-20 19:09:31 +0300

    Updated simulation example to use the new parameter structure

--- examples/random_simulation.py
+++ examples/random_simulation.py
@@ -1,8 +1,7 @@
 from utils.game import CubieDerby
 
 if __name__ == '__main__':
-    playing_cubes = ['Brant', 'Zani', 'Cantarella', 'Phoebe', 'Roccia', 'Cartethyia']
-
-    placement_race = CubieDerby(cubes=playing_cubes, pad_num=23)
-    placement_race.play_game()
+    playing_cubes = ['Jinhsi', 'Shorekeeper', 'Carlotta', 'Calcharo']
+    placement_race = CubieDerby()
+    placement_race.play_game(cubes=playing_cubes, num_of_pads=23)
     placement_race.write_results_to_json('./placement_race_results.json')
